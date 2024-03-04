5456416af0574232454328cd38a04eae3b8d221b
AYHAN GÜNGÖR <gngr.ayhan@gmail.com>
2024-03-04 09:46:46 +0300

    db connect created

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,42 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone"
 
+import { expressMiddleware} from "@apollo/server/express4"
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
+import express from "express"
+import http from "http"
+import cors from "cors"
+import dotenv from "dotenv"
+
 import mergedTypeDefs from "./typeDefs/index.js";
 import mergedResolvers from "./resolvers/index.js";
+
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
+
+const app = express();
+const httpServer = http.createServer(app)
 
 const server = new ApolloServer({
     typeDefs: mergedTypeDefs,
     resolvers: mergedResolvers,
+    plugins:[ApolloServerPluginDrainHttpServer({httpServer})]
 })
 
+await server.start();
 
-const { url } = await startStandaloneServer(server)
+app.use("/",
+    cors(),
+    express.json(),
+    expressMiddleware(server,{
+        context:async({req})=>({req}),
+    })
+)
 
-console.log(`server ready at ${url}`)
+await new Promise((resolve)=>httpServer.listen({port:4000},resolve));
+await connectDB()
+// const { url } = await startStandaloneServer(server)
+
+// console.log(`server ready at ${url}`)
---
+++ backend/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    name:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    profilePicture:{
+        type:String,
+        default:"",
+    },
+    gender:{
+        type:String,
+        enum:["male","female"]
+    }
+},{timestamps:true})
+
+
+const User = mongoose.model("user",userSchema)
+
+export default User
